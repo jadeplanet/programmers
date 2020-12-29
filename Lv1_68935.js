d5c7a1617df874095c1d2eb800d141884fb5a548
jadeplanet <jade_springreen@naver.com>
2020-12-29 22:15:07 +0900

    programmers algorithm

---
+++ Lv1_68935.js
@@ -0,0 +1,19 @@
+// 3진법 뒤집기
+
+let answer = [];
+function solution(n) {
+	let quotient = parseInt(n / 3);
+	let remainder = n % 3;
+	answer.unshift(remainder);
+
+	if (quotient !== 0) {
+		solution(quotient);
+	}
+
+	let result = 0;
+	for (let i = 0; i < answer.length; i++) {
+		let square = 3 ** i;
+		result += answer[i] * square;
+	}
+	return result;
+}
